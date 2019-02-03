639e4ae6f2c0c986d450898b16e9b2ce26eacd72
Matheus Schettino <schettino@Matheuss-MBP.wp.shawcable.net>
2019-02-03 10:38:48 -0600

    Updates

---
+++ precache-manifest.ba20fbead90d2523b82d9d52b324e6da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70b4644b236318b49444",
+    "url": "/static/js/main.70b4644b.chunk.js"
+  },
+  {
+    "revision": "cec250242c2530a0f121",
+    "url": "/static/js/1.cec25024.chunk.js"
+  },
+  {
+    "revision": "8958db8d6f84899d7a981c97c3d6ff8d",
+    "url": "/index.html"
+  }
+];
