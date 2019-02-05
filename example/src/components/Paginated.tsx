5f84e69fc0e3b2d33ce2a56ca92bf41a7b13994e
schettino <schettino2@gmail.com>
2019-02-05 17:30:50 -0600

    Configure example

---
+++ example/src/components/Paginated.tsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {Box, InfiniteScroll, Text} from 'grommet';
+import {useResource} from '../react-request-hook';
+import api, {User} from '../api';
+
+export default function Pagination() {
+  const [usersList, setUsersList] = useState<User[]>([]);
+  const [users, getUsers] = useResource(api.getUsers, []);
+
+  const onMore = useCallback(() => {
+    if (users.data && users.data.page < users.data.total_pages) {
+      getUsers(users.data.page + 1);
+    }
+  }, [users.data]);
+
+  useEffect(() => {
+    if (users.data) {
+      const {data} = users.data;
+      setUsersList(prevItems => [...prevItems, ...data]);
+    }
+  }, [users.data]);
+
+  return (
+    <Box height="small" overflow="auto" pad="medium">
+      <InfiniteScroll step={2} items={usersList} onMore={onMore}>
+        {(user: User) => (
+          <Box
+            flex={false}
+            key={user.id}
+            pad="large"
+            background={`neutral-${(user.id % 4) + 1}`}
+            align="center">
+            <Text size="large" weight="bold" color="white">
+              {JSON.stringify(user)}
+            </Text>
+          </Box>
+        )}
+      </InfiniteScroll>
+    </Box>
+  );
+}
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import {RequestProvider} from './react-request-hook';
 import axios from 'axios';
+import SearchUser, {SearchUserOptmized} from './components/SearchUser';
+import Pagination from './components/Paginated';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://reqres.in/api',
+});
+
+// axiosInstance.interceptors.request.use(config => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(config);
+//       console.count('[REQUEST CALLED]');
+//     }, 6000);
+//   });
+// });
 
 ReactDOM.render(
-  <RequestProvider value={axios}>
-    <App />
+  <RequestProvider value={axiosInstance}>
+    <SearchUser />
+    <SearchUserOptmized />
+    <Pagination />
   </RequestProvider>,
   document.getElementById('root'),
 );
