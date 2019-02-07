84601304e76c4c3726aeea76bb13d0d6f4e1df1c
schettino <schettino2@gmail.com>
2019-02-06 20:08:55 -0600

    Fix imports

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
 import {RequestProvider} from './react-request-hook';
 import axios from 'axios';
-import SearchUser, {SearchUserOptmized} from './components/SearchUser';
 import Pagination from './components/Paginated';
 
 const axiosInstance = axios.create({
   baseURL: 'https://reqres.in/api',
 });
 
-// axiosInstance.interceptors.request.use(config => {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve(config);
-//       console.count('[REQUEST CALLED]');
-//     }, 6000);
-//   });
-// });
-
 ReactDOM.render(
   <RequestProvider value={axiosInstance}>
-    <SearchUser />
-    <SearchUserOptmized />
     <Pagination />
   </RequestProvider>,
   document.getElementById('root'),
