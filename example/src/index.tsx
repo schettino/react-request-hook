39e54db4728a2af5f8f3d7310ceb22a503fd4d2e
schettino <schettino2@gmail.com>
2019-02-16 12:15:57 -0600

    Add router

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {StoreContext} from 'redux-react-hook';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root'),
 );
