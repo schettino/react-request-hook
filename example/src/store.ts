ac14e830ac0fe225034794bd124e1e814349efcc
schettino <schettino2@gmail.com>
2019-02-16 12:16:23 -0600

    Add reducer to cache users list

--- example/src/store.ts
+++ example/src/store.ts
@@ -1,6 +1,30 @@
 import {createStore} from 'redux';
+import {User} from './api';
 
-function reducer(state = null, action: any) {
+const initialState = {
+  usersListCached: {
+    currentPage: 0 as number,
+    data: [] as User[],
+  },
+};
+
+export type CACHE_USERS = {
+  type: 'CACHE_USERS';
+  page: number;
+  data: User[];
+};
+
+export type State = typeof initialState;
+
+function reducer(state = initialState, action: any) {
+  if (action.type === 'CACHE_USERS') {
+    return Object.assign({}, state, {
+      usersListCached: {
+        currentPage: action.page,
+        data: [...state.usersListCached.data, ...action.data],
+      },
+    });
+  }
   return state;
 }
 
