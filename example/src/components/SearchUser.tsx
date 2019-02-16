319f0ebc52ce37613ffa5b68c0015ee568af9589
schettino <schettino2@gmail.com>
2019-02-16 12:17:04 -0600

    Simplified search user

--- example/src/components/SearchUser.tsx
+++ example/src/components/SearchUser.tsx
@@ -1,23 +1,25 @@
 import React, {useState} from 'react';
-import {Box, TextInput} from 'grommet';
+import {TextInput, Image, Text} from 'grommet';
 import {useResource} from '../react-request-hook';
 import useDebounce from '../useDebounce';
 import api from '../api';
+import {Row} from '../styles';
 
 export default function SearchUser() {
   const [input, setInput] = useState<string>('Gabriel');
   const searchText = useDebounce(input);
-  const [issues] = useResource(api.searchUser, [searchText]);
+  const [response] = useResource(api.searchUser, [searchText]);
+
   return (
-    <Box>
-      <TextInput
-        value={input}
-        onChange={(ev: React.ChangeEvent<HTMLInputElement>) => {
-          setInput(ev.target.value);
-        }}
-      />
-      <Box>{JSON.stringify(issues.data)}</Box>
-    </Box>
+    <>
+      <TextInput value={input} onChange={e => setInput(e.target.value)} />
+      {response.data &&
+        response.data.map((user, index) => (
+          <Row key={user.id} alpha={index + 1}>
+            <Image src={user.avatar} />
+            <Text size="large">{user.name}</Text>
+          </Row>
+        ))}
+    </>
   );
 }
-
