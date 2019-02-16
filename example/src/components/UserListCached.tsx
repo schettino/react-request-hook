9152c67b5f53d4bccc2a4aa133ca111b050bda54
schettino <schettino2@gmail.com>
2019-02-16 12:14:50 -0600

    Add example with redux cache

--- example/src/components/UserListCached.tsx
+++ example/src/components/UserListCached.tsx
@@ -1,1 +1,52 @@
-export default () => {};
+import React, {useEffect, useCallback} from 'react';
+import {Box, InfiniteScroll, Text, Image} from 'grommet';
+import {useResource} from '../react-request-hook';
+import api, {User} from '../api';
+import {Row} from '../styles';
+import {useMappedState, useDispatch} from 'redux-react-hook';
+import {State} from '../store';
+
+export const UserListCached: React.FC = () => {
+  const mapStateToProps = useCallback(
+    (state: State) => ({
+      usersList: state.usersListCached.data,
+      page: state.usersListCached.currentPage,
+    }),
+    [],
+  );
+
+  const [response, getUsers] = useResource(api.getUsers);
+  const {page, usersList} = useMappedState(mapStateToProps);
+  const dispatch = useDispatch();
+
+  function onMore() {
+    if (page < 10) {
+      getUsers(page + 1);
+    }
+  }
+
+  useEffect(() => {
+    if (page === 0) {
+      getUsers(1);
+    }
+  }, [])
+
+  useEffect(() => {
+    if (response.data) {
+      dispatch({type: 'CACHE_USERS', page: page + 1, data: response.data});
+    }
+  }, [response.data]);
+
+  return (
+    <Box flex overflow="auto" pad="medium">
+      <InfiniteScroll step={9} items={usersList} onMore={onMore}>
+        {(user: User, index: number) => (
+          <Row key={user.id} alpha={index + 1}>
+            <Image src={user.avatar} />
+            <Text size="large">{user.name}</Text>
+          </Row>
+        )}
+      </InfiniteScroll>
+    </Box>
+  );
+};
