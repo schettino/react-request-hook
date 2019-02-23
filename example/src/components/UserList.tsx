51f8bb6d9abdec5a145e8fab86661b4fb6e2f78e
schettino <schettino2@gmail.com>
2019-02-22 19:38:37 -0600

    Make result really going to infinite

--- example/src/components/UserList.tsx
+++ example/src/components/UserList.tsx
@@ -1,19 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import {Box, InfiniteScroll, Text, Image} from 'grommet';
-import {useResource} from '../react-request-hook';
+import {useResource} from 'react-request-hook';
 import api, {User} from '../api';
 import {Row} from '../styles';
 
 export const UserList: React.FC = () => {
   const [page, setPage] = useState(1);
   const [usersList, setUsersList] = useState<User[]>([]);
-  const [response] = useResource(api.getUsers, [page]);
+  const [response, getUsers] = useResource(api.getUsers);
 
-  function onMore() {
-    if (page < 10) {
-      setPage(p => p + 1);
-    }
-  }
+  const onMore = () => {
+    const nextPage = page + 1;
+    setPage(nextPage > 10 ? 1 : nextPage);
+  };
+
+  useEffect(() => {
+    getUsers(page);
+  }, [page]);
 
   useEffect(() => {
     if (response.data) {
@@ -25,7 +28,7 @@ export const UserList: React.FC = () => {
     <Box flex overflow="auto" pad="medium">
       <InfiniteScroll step={9} items={usersList} onMore={onMore}>
         {(user: User, index: number) => (
-          <Row key={user.id} alpha={index + 1}>
+          <Row key={`${user.id}${index}`} alpha={index + 1}>
             <Image src={user.avatar} />
             <Text size="large">{user.name}</Text>
           </Row>
